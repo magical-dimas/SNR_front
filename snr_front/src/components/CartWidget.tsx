07e2b7a02ae141b2dfe13b8ba814ae2a48e64216
mystic mage <dimbikm@yandex.ru>
2026-04-22 13:19:42 +0300

    small cart fix

--- snr_front/src/components/CartWidget.tsx
+++ snr_front/src/components/CartWidget.tsx
@@ -19,7 +19,7 @@ export const CartWidget: FC = () => {
   return (
     <div className="cart-btn-inactive" title="Текущая заявка">
         <span className="cart_btn_img">&#x2622;</span>
-        <span className="request-icon__badge--inactive">{count}</span>
+        <span className="request-icon__badge--inactive">0</span>
     </div>
   );
 };
